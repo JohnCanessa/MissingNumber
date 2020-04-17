c2cea9df9d23a670d95d2ea2e8ea34545b9a5d22
JohnCanessa <john.canessa@gmail.com>
2020-04-17 15:48:01 -0500

    new approach using shuffled array

---
+++ README.md
@@ -0,0 +1,14 @@
+# ContainsCloseNums
+Missing Number hard problem #17.4 from the book: Cracking the Coding Interview
+by: Gayle Laackmann McDowell.
+Solution using Java developed using the VSCode IDE.
+
+Please refer to the book for requirements and proposed soulution.
+
+For additional information regarding this code please visit my blog post at:
+
+T.B.D.
+
+Enjoy;
+
+John
--- Solution.java
+++ Solution.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
-/*
+
+/**
  * Find missing number in monotonically ascending order array.
+ * 
+ * CORRECTION: Numbers are NOT in sorted order in the array.
+ * 
  * The array starts with value zero (0).
  * Problem 17.4
  */
 public class Solution {
 
-	/*
+
+	/**
 	 * Fetch jth bit from arr[i]
 	 */
 	static int fetchBit(int[] arr, int i, int j) {
@@ -29,11 +37,12 @@ public class Solution {
 		return arr[i] & mask;
 	}
 	
-	/*
+
+	/**
 	 * Find missing value in O(n).
 	 * arr[i] & 0x01 is equivalent to fetch the 0th bit (LSB) from arr[i].
 	 */
-	static int findMissing(int[] arr) {
+	static int findMissingSorted(int[] arr) {
 		
 		// **** ****
 		int missing = 0;
@@ -49,8 +58,64 @@ public class Solution {
 		// **** ****
 		return missing;
 	}
-	
-	/*
+
+
+	/**
+	 * Converts array to array list.
+	 * Start recursion.
+	 */
+	static int findMissing(int[] arr) {
+
+		// **** ****
+		ArrayList<Integer> al = new ArrayList<Integer>();;
+
+		// **** populate array list ****
+		for (int i = 0; i < arr.length; i++)
+			al.add(arr[i]);
+
+		// **** start recursion with LSB ****
+		return findMissing(al, 0);
+	}
+
+
+	/**
+	 * Recursive call.
+	 */
+	static int findMissing(ArrayList<Integer> al, int lsb) {
+
+		// **** end / stop condition ****
+		if (lsb >= Integer.SIZE)
+			return 0;
+
+		// **** ****
+		ArrayList<Integer> oneBits = new ArrayList<Integer>();
+		ArrayList<Integer> zeroBits = new ArrayList<Integer>();
+
+		// **** set mask ****
+		int mask = 1;
+		mask <<= lsb;
+
+		// **** populate array lists ****
+		for (Integer i : al) {
+			if ((i & mask) == 0) {
+				zeroBits.add(i);
+			} else {
+				oneBits.add(i);
+			}
+		}
+
+		// **** recursive call ****
+		if (zeroBits.size() <= oneBits.size()) {
+			int v = findMissing(zeroBits, lsb + 1);
+			return (v << 1) | 0;
+		} else {
+			int v = findMissing(oneBits, lsb + 1);
+			return (v << 1) | 1;
+		}
+	}
+
+
+	/**
 	 * Test scaffolding.
 	 * random.nextInt(max - min + 1) + min
 	 */
@@ -60,53 +125,74 @@ public class Solution {
 		final int MAX_LENGTH = 19;
 		final int MIN_LENGTH = 2;
 		
-		// **** determine n ****
+		// **** ****
 		Random rand = new Random(System.currentTimeMillis());
+
+		// **** determine n ****
 		int n = Math.abs(rand.nextInt(MAX_LENGTH - MIN_LENGTH + 1)) + MIN_LENGTH;
 		
 		// ???? ????
 		System.out.println("main <<<   n: " + n);
-		
-		// **** array holding missing number ****
-		int[] arr = new int[n];
 
-		// **** determine missing value ****
+		// **** determine the missing value ****
 		int m = Math.abs(rand.nextInt((n - 1) - 0 + 1) + 0);
 
 		// ???? ????
 		System.out.println("main <<<   m: " + m);
 
-		// **** populate array without the missing value ****
+		// **** declare array list ****
+		ArrayList<Integer> al = new ArrayList<Integer>();
+
+		// **** populate array list with a missing number ****
 		int j = 0;
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < n; i++) {
 			
 			// **** skip this value ****
 			if (i == m)
 				j++;
 			
 			// **** populate array with this value ****
-			arr[i] = j++;
+			al.add(j++);
 		}
-		
-		// ???? ????
-		System.out.print("main <<< arr: [");
-		for (int i = 0; i < arr.length; i++) {
-			if (i < arr.length - 1)
-				System.out.print(arr[i] + ", ");
-			else 
-				System.out.print(arr[i]);			
-		}
-		System.out.println("] arr.length: " + arr.length);
-		
-		// **** find missing value ****
-		int missing = findMissing(arr);
 
-		// **** check if we found the proper missing value ****
-		System.out.println();
+		// **** populate array with the contents of the array list ****
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = al.get(i);
+			
+		// ???? ????
+		System.out.println("main <<< arr: " + Arrays.toString(arr));
+
+		// **** missing number ****
+		int missing;
+
+		// **** find the missing number ****
+		missing = findMissingSorted(arr);
+
+		// **** check if we found the proper missing number ****
 		if (missing == m)
 			System.out.println("main <<< missing: " + missing + " == m: " + m);
 		else
-			System.err.println("main <<< missing: " + missing + " != m: " + m);
-	}
+			System.err.println("main <<< missing: " + missing + " != m: " + m + " :o(");
 
+		// **** shuffle the array list ****
+		Collections.shuffle(al);
+
+		// **** populate array with the contents of the array list ****
+		arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = al.get(i);
+			
+		// ???? display the shuffled array ????
+		System.out.println("main <<< arr: " + Arrays.toString(arr));
+
+		// **** find the missing number ****
+		missing = findMissing(arr);
+
+		// **** check if we found the proper missing number ****
+		if (missing == m)
+			System.out.println("main <<< missing: " + missing + " == m: " + m);
+		else
+			System.err.println("main <<< missing: " + missing + " != m: " + m + " :o(");
+	}
 }
